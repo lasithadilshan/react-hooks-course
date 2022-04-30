104f5588661f0c5451d334b10a59597fc7afe288
lasithadilshan <dilshantilakaratne29@gmail.com>
2022-04-30 07:00:32 +0530

    1 file modified number increment works

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from "react";
-import "./style.css";
+import React, {useState} from 'react';
+import './style.css';
 
 const App = () => {
-    return <div> Hello World</div>;
+  const [counter, setCounter] = useState(0);
+
+  const Increment = () => {
+    setCounter(counter+1);
+  };
+  return (
+    <div>
+      {counter} <button onClick={Increment}>Increment</button>
+    </div>
+  );
 };
 
 export default App;
