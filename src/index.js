e1649496fc1390ce515b8ec7b5544843a4fcc14b
lasithadilshan <dilshantilakaratne29@gmail.com>
2022-05-04 18:49:27 +0530

    3 files modified, 1 added EffectTutorial completed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import ReducerTutorial from './ReducerTutorial';
+import EffectTutorial from './EffectTutorial';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <ReducerTutorial />
+    <EffectTutorial />
   </StrictMode>
 );
